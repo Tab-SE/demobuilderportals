7a14a7217e82f0f08a36ed0687ec5ebfdac6e212
alexeski <8482762+alexeski@users.noreply.github.com>
2023-09-21 10:24:18 +0100

    testing new branch

---
+++ myNewPortal/lib/config.js
@@ -0,0 +1,54 @@
+var tab_server = [
+  "https://public.tableau.com/views/MarvelComicUniverse_16596317882150/Marvel",
+  "",
+  "",
+  "",
+];
+var tab_filter = [["FirstLetter"], ["FirstLetter"], [], []];
+var tab_web = [
+  { key: "1", val: "false" },
+  { key: "0", val: "true" },
+  { key: "2", val: "false" },
+  { key: "3", val: "false" },
+];
+var tab_ask = [
+  { key: "0", val: "" },
+  { key: "3", val: "" },
+  { key: "1", val: "" },
+  { key: "2", val: "" },
+];
+var tab_param = [[], ["Starts with"], [], []];
+var tab_text = [
+  { key: "Filter-text-FirstLetter", val: "Choose%20first%20letter" },
+  { key: "Viz-3-text", val: "Viz%203%20" },
+  { key: "Viz-2-text", val: "Viz%202" },
+  { key: "Filter-text-Starts with", val: "Character%20First%20Letter" },
+  { key: "WebEdit-text", val: "Web%20Edit%20" },
+  { key: "Download-Data-text", val: "Download%20Data%20" },
+  { key: "User-text", val: "Spider%20Man" },
+  { key: "Action-text", val: "Action%20" },
+  { key: "Viz-1-text", val: "Marvel" },
+  { key: "Viz-4-text", val: "Viz%204%20" },
+  { key: "Reset-Viz-text", val: "Reset%20Viz%20" },
+  { key: "PrivacyPolicy-text", val: "Privacy%20Policy%20" },
+  { key: "Filter-text-Comics/Series", val: "Choose%20Series" },
+  { key: "Terms-text", val: "Terms%20%26%20Conditions%20" },
+  { key: "AskData-text", val: "Ask%20data%20" },
+];
+var tab_img = [
+  {
+    key: "img-logo",
+    val: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Marvel_Studios_2016_logo.svg/1280px-Marvel_Studios_2016_logo.svg.png",
+  },
+  {
+    key: "img-avatar",
+    val: "https://icon-library.com/images/spider-man-icon/spider-man-icon-16.jpg",
+  },
+];
+var tab_action = [
+  { key: "0", val: "true" },
+  { key: "2", val: "false" },
+  { key: "1", val: "false" },
+  { key: "3", val: "false" },
+];
+var title_index = "Marvel Comics";
