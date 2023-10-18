1964645bbb0456e6abf4280b01edd619c5c4b590
alexeski <8482762+alexeski@users.noreply.github.com>
2023-10-18 15:54:59 +0100

    Add Hubway Bike Sharing portal

---
+++ hubway/lib/config.js
@@ -0,0 +1,52 @@
+var tab_server = [
+  "https://public.tableau.com/views/HubwayStationAnalysis_16976402496650/Hubway-StationtoStation",
+  "https://public.tableau.com/views/HubwayStationAnalysis_16976402496650/Map",
+  "",
+  "",
+];
+var tab_filter = [[], [], [], []];
+var tab_web = [
+  { key: "0", val: "true" },
+  { key: "1", val: "false" },
+  { key: "2", val: "false" },
+  { key: "3", val: "false" },
+];
+var tab_ask = [
+  { key: "3", val: "" },
+  { key: "1", val: "" },
+  { key: "2", val: "" },
+  { key: "0", val: "" },
+];
+var tab_param = [["Station"], [], [], []];
+var tab_text = [
+  { key: "Download-Data-text", val: "Download%20Data%20" },
+  { key: "Viz-4-text", val: "Viz%204%20" },
+  { key: "Reset-Viz-text", val: "Reset%20Viz%20" },
+  { key: "AskData-text", val: "Ask%20data%20" },
+  { key: "Viz-2-text", val: "Map" },
+  { key: "WebEdit-text", val: "Web%20Edit%20" },
+  { key: "User-text", val: "Alex%2C%20Admin" },
+  { key: "Viz-3-text", val: "Viz%203%20" },
+  { key: "Terms-text", val: "Terms%20%26%20Conditions%20" },
+  { key: "Action-text", val: "Action%20" },
+  { key: "Viz-1-text", val: "Hubway%20Dashboard" },
+  { key: "Filter-text-Station", val: "Station" },
+  { key: "PrivacyPolicy-text", val: "Privacy%20Policy%20" },
+];
+var tab_img = [
+  {
+    key: "img-avatar",
+    val: "https://tab-se.github.io/demobuilderportals/assets/user_avatar.png",
+  },
+  {
+    key: "img-logo",
+    val: "https://www.pngkey.com/png/full/976-9767075_hubway-curative-cupping.png",
+  },
+];
+var tab_action = [
+  { key: "3", val: "false" },
+  { key: "2", val: "false" },
+  { key: "1", val: "false" },
+  { key: "0", val: "true" },
+];
+var title_index = "hubway";
