bcf4f15b42cc18e20b5efa6fc7bfa1c0b93ee383
alexeski <8482762+alexeski@users.noreply.github.com>
2023-10-19 11:03:20 +0100

    Update ESG portals

--- ESG_Slalom/lib/config.js
+++ ESG_Slalom/lib/config.js
@@ -1,9 +1,52 @@
-var tab_server = ["https://dub01.online.tableau.com/t/eskissite/views/ESGImpactTrackingAccelerator/EnvironmentalImpactTracking","https://dub01.online.tableau.com/t/eskissite/views/ESGImpactTrackingAccelerator/SocialImpactTracking","https://dub01.online.tableau.com/t/eskissite/views/SustainableSupplyChain/SustainableSupplyChain",""];
-  var tab_filter=[[],[],[],[]];
-  var tab_web=[{"key":"1","val":"true"},{"key":"0","val":"true"},{"key":"3","val":"false"},{"key":"2","val":"true"}];
-  var tab_ask=[{"key":"0","val":""},{"key":"3","val":""},{"key":"1","val":""},{"key":"2","val":""}];
-  var tab_param=[[],[],[],[]]; 
-  var tab_text=[{"key":"AskData-text","val":"Ask%20data%20"},{"key":"Viz-2-text","val":"Social%20Impact%20Tracking"},{"key":"WebEdit-text","val":"Web%20Edit%20"},{"key":"Viz-3-text","val":"Sustainable%20Supply%20Chain"},{"key":"PrivacyPolicy-text","val":"Privacy%20Policy%20"},{"key":"Action-text","val":"Action%20"},{"key":"Download-Data-text","val":"Download%20Data%20"},{"key":"Viz-4-text","val":"Viz%204%20"},{"key":"Filter-text-Product","val":"Product"},{"key":"Reset-Viz-text","val":"Reset%20Viz%20"},{"key":"User-text","val":"User%20"},{"key":"Terms-text","val":"Terms%20%26%20Conditions%20"},{"key":"Viz-1-text","val":"Environmental%20Impact%20Tracking"}];
-  var tab_img=[{"key":"img-avatar","val":"https://assets.webiconspng.com/uploads/2016/12/User-Icon.png"},{"key":"img-logo","val":"https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Logo_acme.svg/2560px-Logo_acme.svg.png"}];
-  var tab_action=[{"key":"3","val":"false"},{"key":"2","val":"true"},{"key":"0","val":"true"},{"key":"1","val":"true"}];
-  var title_index="ESG_Slalom";
+var tab_server = [
+  "https://public.tableau.com/views/ESGImpactTrackingAccelerator_16977095159320/EnvironmentalImpactTracking",
+  "https://public.tableau.com/views/ESGImpactTrackingAccelerator_16977095159320/SocialImpactTracking",
+  "https://public.tableau.com/views/SustainableSupplyChain1/SustainableSupplyChain",
+  "",
+];
+var tab_filter = [[], [], [], []];
+var tab_web = [
+  { key: "1", val: "true" },
+  { key: "0", val: "true" },
+  { key: "3", val: "false" },
+  { key: "2", val: "true" },
+];
+var tab_ask = [
+  { key: "0", val: "" },
+  { key: "3", val: "" },
+  { key: "1", val: "" },
+  { key: "2", val: "" },
+];
+var tab_param = [[], [], [], []];
+var tab_text = [
+  { key: "AskData-text", val: "Ask%20data%20" },
+  { key: "Viz-2-text", val: "Social%20Impact%20Tracking" },
+  { key: "WebEdit-text", val: "Web%20Edit%20" },
+  { key: "Viz-3-text", val: "Sustainable%20Supply%20Chain" },
+  { key: "PrivacyPolicy-text", val: "Privacy%20Policy%20" },
+  { key: "Action-text", val: "Action%20" },
+  { key: "Download-Data-text", val: "Download%20Data%20" },
+  { key: "Viz-4-text", val: "Viz%204%20" },
+  { key: "Filter-text-Product", val: "Product" },
+  { key: "Reset-Viz-text", val: "Reset%20Viz%20" },
+  { key: "User-text", val: "User%20" },
+  { key: "Terms-text", val: "Terms%20%26%20Conditions%20" },
+  { key: "Viz-1-text", val: "Environmental%20Impact%20Tracking" },
+];
+var tab_img = [
+  {
+    key: "img-logo",
+    val: "https://www.riacanada.ca/content/uploads/2020/01/ESG_Logo.png",
+  },
+  {
+    key: "img-avatar",
+    val: "https://tab-se.github.io/demobuilderportals/assets/user_avatar.png",
+  },
+];
+var tab_action = [
+  { key: "3", val: "false" },
+  { key: "2", val: "true" },
+  { key: "0", val: "true" },
+  { key: "1", val: "true" },
+];
+var title_index = "ESG_Slalom";
--- ESG_Tableau/lib/config.js
+++ ESG_Tableau/lib/config.js
@@ -1,9 +1,51 @@
-var tab_server = ["https://dub01.online.tableau.com/t/eskissite/views/ESG-ESGDashboard/Cockpit","https://dub01.online.tableau.com/t/eskissite/views/ESG-ESGDashboard/Environment","https://dub01.online.tableau.com/t/eskissite/views/ESG-ESGDashboard/CompanyDetail","https://dub01.online.tableau.com/t/eskissite/views/ESG-ESGDashboard/Analysis-Adhoc"];
-  var tab_filter=[[],[],[],[]];
-  var tab_web=[{"key":"1","val":"true"},{"key":"2","val":"true"},{"key":"0","val":"true"},{"key":"3","val":"true"}];
-  var tab_ask=[{"key":"3","val":""},{"key":"0","val":""},{"key":"1","val":""},{"key":"2","val":""}];
-  var tab_param=[[],[],[],[]]; 
-  var tab_text=[{"key":"Download-Data-text","val":"Download%20Data%20"},{"key":"User-text","val":"Alex"},{"key":"Action-text","val":"Action%20"},{"key":"Terms-text","val":"Terms%20%26%20Conditions%20"},{"key":"Viz-1-text","val":"Cockpit"},{"key":"WebEdit-text","val":"Web%20Edit%20"},{"key":"Viz-4-text","val":"Analysis%20-%20Adhoc"},{"key":"Reset-Viz-text","val":"Reset%20Viz%20"},{"key":"AskData-text","val":"Ask%20data%20"},{"key":"PrivacyPolicy-text","val":"Privacy%20Policy%20"},{"key":"Viz-3-text","val":"Company%20Detail"},{"key":"Viz-2-text","val":"Environment"}];
-  var tab_img=[{"key":"img-logo","val":"https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Logo_acme.svg/2560px-Logo_acme.svg.png"},{"key":"img-avatar","val":"https://assets.webiconspng.com/uploads/2016/12/User-Icon.png"}];
-  var tab_action=[{"key":"2","val":"true"},{"key":"1","val":"true"},{"key":"3","val":"true"},{"key":"0","val":"true"}];
-  var title_index="ESG_Tableau";
+var tab_server = [
+  "https://public.tableau.com/views/ESGBenchmark_16975609693400/ExecutiveSummary",
+  "https://public.tableau.com/views/ESGBenchmark_16975609693400/Environment",
+  "https://public.tableau.com/views/ESGBenchmark_16975609693400/Company",
+  "https://public.tableau.com/views/ESGBenchmark_16975609693400/Adhoc",
+];
+var tab_filter = [[], [], [], []];
+var tab_web = [
+  { key: "1", val: "true" },
+  { key: "2", val: "true" },
+  { key: "0", val: "true" },
+  { key: "3", val: "true" },
+];
+var tab_ask = [
+  { key: "3", val: "" },
+  { key: "0", val: "" },
+  { key: "1", val: "" },
+  { key: "2", val: "" },
+];
+var tab_param = [[], [], [], []];
+var tab_text = [
+  { key: "Download-Data-text", val: "Download%20Data%20" },
+  { key: "User-text", val: "Alex" },
+  { key: "Action-text", val: "Action%20" },
+  { key: "Terms-text", val: "Terms%20%26%20Conditions%20" },
+  { key: "Viz-1-text", val: "Cockpit" },
+  { key: "WebEdit-text", val: "Web%20Edit%20" },
+  { key: "Viz-4-text", val: "Analysis%20-%20Adhoc" },
+  { key: "Reset-Viz-text", val: "Reset%20Viz%20" },
+  { key: "AskData-text", val: "Ask%20data%20" },
+  { key: "PrivacyPolicy-text", val: "Privacy%20Policy%20" },
+  { key: "Viz-3-text", val: "Company%20Detail" },
+  { key: "Viz-2-text", val: "Environment" },
+];
+var tab_img = [
+  {
+    key: "img-logo",
+    val: "https://www.riacanada.ca/content/uploads/2020/01/ESG_Logo.png",
+  },
+  {
+    key: "img-avatar",
+    val: "https://tab-se.github.io/demobuilderportals/assets/user_avatar.png",
+  },
+];
+var tab_action = [
+  { key: "2", val: "true" },
+  { key: "1", val: "true" },
+  { key: "3", val: "true" },
+  { key: "0", val: "true" },
+];
+var title_index = "ESG_Tableau";
