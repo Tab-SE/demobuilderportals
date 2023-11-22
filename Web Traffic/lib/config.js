ab4725a00c6c6eb47862f677c10cc0950370d002
GLaw888 <151741350+GLaw888@users.noreply.github.com>
2023-11-22 12:45:48 +0000

    Add files via upload

---
+++ Web Traffic/lib/config.js
@@ -0,0 +1,9 @@
+var tab_server = ["https://public.tableau.com/views/GLWebTrafficCockpit/Cockpit?:showVizHome=no&:embed=true","https://public.tableau.com/views/GLWebTrafficTopPerformers/TopPerformers?:showVizHome=no&:embed=true","",""];
+  var tab_filter=[["Channel Grouping","Country","Device Category"],["Channel Grouping","Country","Device Category"],[],[]];
+  var tab_web=[{"key":"0","val":"false"},{"key":"1","val":"false"},{"key":"3","val":"false"},{"key":"2","val":"false"}];
+  var tab_ask=[{"key":"3","val":""},{"key":"1","val":""},{"key":"2","val":""},{"key":"0","val":""}];
+  var tab_param=[[],[],[],[]]; 
+  var tab_text=[{"key":"Download-Data-text","val":"Download%20Data%20"},{"key":"Filter-text-Country","val":"Country"},{"key":"Viz-4-text","val":"Viz%204%20"},{"key":"Reset-Viz-text","val":"Reset%20Viz%20"},{"key":"AskData-text","val":"Ask%20data%20"},{"key":"Viz-2-text","val":"Top%20Performers"},{"key":"Filter-text-Device Category","val":"Device%20Category"},{"key":"Filter-text-Channel Grouping","val":"Channel%20Grouping"},{"key":"WebEdit-text","val":"Web%20Edit%20"},{"key":"User-text","val":"User%20"},{"key":"Viz-3-text","val":"Viz%203%20"},{"key":"Terms-text","val":"Terms%20%26%20Conditions%20"},{"key":"PrivacyPolicy-text","val":"Privacy%20Policy%20"},{"key":"Viz-1-text","val":"Cockpit"},{"key":"Action-text","val":"Action%20"}];
+  var tab_img=[{"key":"img-logo","val":"https://cdn-icons-png.flaticon.com/512/6578/6578820.png"},{"key":"img-avatar","val":"https://tab-se.github.io/demobuilderportals/assets/user_avatar.png"}];
+  var tab_action=[{"key":"3","val":"false"},{"key":"0","val":"true"},{"key":"2","val":"false"},{"key":"1","val":"false"}];
+  var title_index="Web Traffic";
