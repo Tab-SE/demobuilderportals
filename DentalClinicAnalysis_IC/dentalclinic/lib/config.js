0acffe6f2ee0f89a19bcb36d7397856e160f6d13
Iconrado24 <151923027+Iconrado24@users.noreply.github.com>
2023-11-24 15:48:35 +0100

    Delete DentalClinicAnalysis_IC/dentalclinic directory

--- DentalClinicAnalysis_IC/dentalclinic/lib/config.js
+++
@@ -1,9 +0,0 @@
-var tab_server = ["https://public.tableau.com/views/ClinicControlCenter/DentalControlCenter?:showVizHome=no&:embed=true","https://public.tableau.com/views/PatientNoShowRisk/PredictedNoShows?:showVizHome=no&:embed=true","",""];
-  var tab_filter=[["Country"],[],[],[]];
-  var tab_web=[{"key":"1","val":"false"},{"key":"2","val":"false"},{"key":"0","val":"false"},{"key":"3","val":"false"}];
-  var tab_ask=[{"key":"3","val":""},{"key":"0","val":""},{"key":"1","val":""},{"key":"2","val":""}];
-  var tab_param=[[],["Appointment Location ","Distance to Clinic Parameter"],[],[]]; 
-  var tab_text=[{"key":"User-text","val":"User%20"},{"key":"Download-Data-text","val":"Download%20Data%20"},{"key":"Action-text","val":"Action%20"},{"key":"Filter-text-Appointment Location ","val":"Appointment%20Location%20"},{"key":"Viz-1-text","val":"DentalControlCenter"},{"key":"WebEdit-text","val":"Web%20Edit%20"},{"key":"Terms-text","val":"Terms%20%26%20Conditions%20"},{"key":"Filter-text-Country","val":"Country"},{"key":"Viz-4-text","val":"Viz%204%20"},{"key":"Reset-Viz-text","val":"Reset%20Viz%20"},{"key":"AskData-text","val":"Ask%20data%20"},{"key":"Filter-text-Distance to Clinic Parameter","val":"Distance%20to%20Clinic%20Parameter"},{"key":"PrivacyPolicy-text","val":"Privacy%20Policy%20"},{"key":"Viz-3-text","val":"Viz%203%20"},{"key":"Viz-2-text","val":"PredictedNoShows"}];
-  var tab_img=[{"key":"img-logo","val":"https://tab-se.github.io/demobuilderportals/assets/TABlogo_wht.png"},{"key":"img-avatar","val":"https://tab-se.github.io/demobuilderportals/assets/user_avatar.png"}];
-  var tab_action=[{"key":"2","val":"false"},{"key":"1","val":"true"},{"key":"0","val":"false"},{"key":"3","val":"true"}];
-  var title_index="dentalclinic";
