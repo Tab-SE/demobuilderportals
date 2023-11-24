f2c2d002d02dff9258c51ea670ddde4428fd6be9
Peter Jönsson <44160703+Sweden21@users.noreply.github.com>
2023-11-24 18:29:19 +0100

    Sales Metrics 2023 (peter jonsson)

---
+++ PJ_SALES_METRICS_2023/lib/config.js
@@ -0,0 +1,9 @@
+var tab_server = ["https://public.tableau.com/views/PJ_Sales_Metrics_2023_01/PJ_Sales_Metrics_2023_01?:showVizHome=no&:embed=true","https://public.tableau.com/views/PJ_Sales_Metrics_2023_02/PJ_Sales_Metrics_2023_02?:showVizHome=no&:embed=true","",""];
+  var tab_filter=[["CATEGORY"],["SHIP MODE"],[],[]];
+  var tab_web=[{"key":"0","val":"false"},{"key":"1","val":"false"},{"key":"3","val":"false"},{"key":"2","val":"false"}];
+  var tab_ask=[{"key":"3","val":""},{"key":"1","val":""},{"key":"2","val":""},{"key":"0","val":""}];
+  var tab_param=[[],[],[],[]]; 
+  var tab_text=[{"key":"Download-Data-text","val":"Download%20Data%20"},{"key":"Viz-4-text","val":"Viz%204%20"},{"key":"Filter-text-SHIP MODE","val":"SHIP%20MODE"},{"key":"Reset-Viz-text","val":"Reset%20Viz%20"},{"key":"AskData-text","val":"Ask%20data%20"},{"key":"Viz-2-text","val":"Sales%20by%20City"},{"key":"WebEdit-text","val":"Web%20Edit%20"},{"key":"Filter-text-CATEGORY","val":"CATEGORY"},{"key":"User-text","val":"Peter%20J%C3%B6nsson%20"},{"key":"Viz-3-text","val":"Viz%203%20"},{"key":"Terms-text","val":"Terms%20%26%20Conditions%20"},{"key":"PrivacyPolicy-text","val":"Privacy%20Policy%20"},{"key":"Viz-1-text","val":"Product%20Sales"},{"key":"Action-text","val":"Action%20"}];
+  var tab_img=[{"key":"img-logo","val":"https://tab-se.github.io/demobuilderportals/assets/TABlogo_wht.png"},{"key":"img-avatar","val":"https://tab-se.github.io/demobuilderportals/assets/user_avatar.png"}];
+  var tab_action=[{"key":"3","val":"false"},{"key":"0","val":"true"},{"key":"2","val":"false"},{"key":"1","val":"false"}];
+  var title_index="PJ_SALES_METRICS_2023";
