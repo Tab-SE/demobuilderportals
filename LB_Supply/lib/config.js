ac53e497c8d386abe1591a55e4bed4814c57e35b
lboehnke <52285417+lboehnke@users.noreply.github.com>
2023-11-24 14:10:37 +0100

    Add files via upload

---
+++ LB_Supply/lib/config.js
@@ -0,0 +1,9 @@
+var tab_server = ["https://public.tableau.com/views/LB_Supply/WHMetrics?:showVizHome=no&:embed=true","https://public.tableau.com/views/LB_Supply_Manifest/Manifest?:showVizHome=no&:embed=true","",""];
+  var tab_filter=[[],[],[],[]];
+  var tab_web=[{"key":"0","val":"false"},{"key":"1","val":"false"},{"key":"3","val":"false"},{"key":"2","val":"false"}];
+  var tab_ask=[{"key":"3","val":""},{"key":"1","val":""},{"key":"2","val":""},{"key":"0","val":""}];
+  var tab_param=[["Choose WH"],[],[],[]]; 
+  var tab_text=[{"key":"Download-Data-text","val":"Download%20Data%20"},{"key":"Viz-4-text","val":"Viz%204%20"},{"key":"Reset-Viz-text","val":"Reset%20Viz%20"},{"key":"AskData-text","val":"Ask%20data%20"},{"key":"Viz-2-text","val":"Warehouse%20Details"},{"key":"WebEdit-text","val":"Web%20Edit%20"},{"key":"User-text","val":"Lena"},{"key":"Filter-text-Choose WH","val":"Choose%20WH"},{"key":"Viz-3-text","val":"Viz%203%20"},{"key":"Terms-text","val":"Terms%20%26%20Conditions%20"},{"key":"PrivacyPolicy-text","val":"Privacy%20Policy%20"},{"key":"Viz-1-text","val":"KPI%20Overview"},{"key":"Action-text","val":"Action%20"}];
+  var tab_img=[{"key":"img-logo","val":"https://tab-se.github.io/demobuilderportals/assets/TABlogo_wht.png"},{"key":"img-avatar","val":"https://tab-se.github.io/demobuilderportals/assets/user_avatar.png"}];
+  var tab_action=[{"key":"3","val":"false"},{"key":"0","val":"true"},{"key":"2","val":"false"},{"key":"1","val":"false"}];
+  var title_index="LB_Supply";
