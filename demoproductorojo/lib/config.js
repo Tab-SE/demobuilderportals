95e58154d9c868ad2a7be2c442f151b65d979373
Pablo621 <figaredo@gmail.com>
2024-01-02 16:05:06 +0100

    Add files via upload

---
+++ demoproductorojo/lib/config.js
@@ -0,0 +1,9 @@
+var tab_server = ["https://public.tableau.com/views/Informe_Producto/Informe_Producto?:showVizHome=no&:embed=true","https://public.tableau.com/views/Informe_Tiendas/Informe_Tiendas?:showVizHome=no&:embed=true","",""];
+  var tab_filter=[["Categoria","Comunidad Autónoma"],["Categoria"],[],[]];
+  var tab_web=[{"key":"0","val":"false"},{"key":"1","val":"false"},{"key":"3","val":"false"},{"key":"2","val":"false"}];
+  var tab_ask=[{"key":"3","val":""},{"key":"1","val":""},{"key":"2","val":""},{"key":"0","val":""}];
+  var tab_param=[[],[],[],[]]; 
+  var tab_text=[{"key":"Filter-text-Categoria","val":"Categoria"},{"key":"Download-Data-text","val":"Descargar%20Datos"},{"key":"Viz-4-text","val":"Viz%204%20"},{"key":"Reset-Viz-text","val":"Reiniciar"},{"key":"AskData-text","val":"Pregunta%20a%20los%20datos"},{"key":"Viz-2-text","val":"Tiendas"},{"key":"WebEdit-text","val":"Edici%C3%B3n%20web"},{"key":"User-text","val":"Pablo%20F.%20"},{"key":"Viz-3-text","val":"Viz%203%20"},{"key":"Terms-text","val":"T%C3%A9rminos%20y%20Condiciones"},{"key":"PrivacyPolicy-text","val":"P%C3%B3liza%20de%20privacidad"},{"key":"Viz-1-text","val":"Producto"},{"key":"Action-text","val":"Acci%C3%B3n"},{"key":"Filter-text-Comunidad Autónoma","val":"Comunidad%20Aut%C3%B3noma"}];
+  var tab_img=[{"key":"img-logo","val":"https://previews.123rf.com/images/yupiramos/yupiramos1505/yupiramos150502897/40417542-dise%C3%B1o-de-seguros-sobre-fondo-blanco-ilustraci%C3%B3n-vectorial.jpg"},{"key":"img-avatar","val":"https://cdn-icons-png.flaticon.com/512/1198/1198293.png"}];
+  var tab_action=[{"key":"3","val":"false"},{"key":"0","val":"true"},{"key":"2","val":"false"},{"key":"1","val":"true"}];
+  var title_index="DemoProductoRojo";
